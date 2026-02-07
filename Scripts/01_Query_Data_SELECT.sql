b262b9a02f239f7220bb12291b051f278998c469
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-02-07 12:21:19 +0530

    SQL SELECT
    This SQL script provides various techniques for SELECT queries, including filtering, sorting, and aggregating data.

---
+++ Scripts/01_Query_Data_SELECT.sql
@@ -0,0 +1,43 @@
+/* ==============================================================================
+   SQL SELECT Query
+-------------------------------------------------------------------------------
+   This guide covers various SELECT query techniques used for retrieving, 
+   filtering, sorting, and aggregating data efficiently.
+
+   Table of Contents:
+     1. SELECT ALL COLUMNS
+     2. SELECT SPECIFIC COLUMNS
+     3. WHERE CLAUSE
+     4. ORDER BY
+     5. GROUP BY
+     6. HAVING
+     7. DISTINCT
+     8. TOP
+     9. Combining Queries
+	 10. COOL STUFF - Additional SQL Features
+=================================================================================
+*/
+
+/* ==============================================================================
+   COMMENTS
+=============================================================================== */
+
+-- This is a single-line comment.
+
+/* This
+   is
+   a multiple-line
+   comment
+*/
+
+/* ==============================================================================
+   SELECT ALL COLUMNS
+=============================================================================== */
+
+-- Retrieve All Customer Data
+SELECT *
+FROM customers
+
+-- Retrieve All Order Data
+SELECT *
+FROM orders
