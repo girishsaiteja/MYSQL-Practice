4c9590be4845a9d7cfdd844e87903d5fed697ae9
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-02-15 22:33:46 +0530

    Update 01_Query_Data_SELECT.sql

--- Scripts/01_Query_Data_SELECT.sql
+++ Scripts/01_Query_Data_SELECT.sql
@@ -54,3 +54,25 @@ SELECT
     score
 FROM customers
 
+/* ==============================================================================
+   WHERE
+=============================================================================== */
+
+-- Retrieve customers with a score not equal to 0
+SELECT *
+FROM customers
+WHERE score != 0
+
+-- Retrieve customers from Germany
+SELECT *
+FROM customers
+WHERE country = 'Germany'
+
+-- Retrieve the name and country of customers from Germany
+SELECT
+    first_name,
+    country
+FROM customers
+WHERE country = 'Germany'
+
+
