5abc67d1c9dc212e0dad5be1ba327ef159350105
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-02-16 10:41:43 +0530

    Add queries to sort customers by score and country
    Added multiple SELECT queries to retrieve customers sorted by score and country.

--- Scripts/01_Query_Data_SELECT.sql
+++ Scripts/01_Query_Data_SELECT.sql
@@ -75,4 +75,42 @@ SELECT
 FROM customers
 WHERE country = 'Germany'
 
+/* ==============================================================================
+   ORDER BY
+=============================================================================== */
+
+/* Retrieve all customers and 
+   sort the results by the highest score first. */
+SELECT *
+FROM customers
+ORDER BY score DESC
+
+/* Retrieve all customers and 
+   sort the results by the lowest score first. */
+SELECT *
+FROM customers
+ORDER BY score ASC
+
+/* Retrieve all customers and 
+   sort the results by the country. */
+SELECT *
+FROM customers
+ORDER BY country ASC
+
+/* Retrieve all customers and 
+   sort the results by the country and then by the highest score. */
+SELECT *
+FROM customers
+ORDER BY country ASC, score DESC
+
+/* Retrieve the name, country, and score of customers 
+   whose score is not equal to 0
+   and sort the results by the highest score first. */
+SELECT
+    first_name,
+    country,
+    score
+FROM customers
+WHERE score != 0
+ORDER BY score DESC
 
