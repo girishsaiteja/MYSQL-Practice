2df31d66ea9ebbd69a042d264bc542136f98199f
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-02-17 13:34:42 +0530

    Add SQL queries for grouping and aggregating scores

--- Scripts/01_Query_Data_SELECT.sql
+++ Scripts/01_Query_Data_SELECT.sql
@@ -114,3 +114,30 @@ FROM customers
 WHERE score != 0
 ORDER BY score DESC
 
+/* ==============================================================================
+   GROUP BY
+=============================================================================== */
+
+-- Find the total score for each country
+SELECT 
+    country,
+    SUM(score) AS total_score
+FROM customers
+GROUP BY country
+
+/* This will not work because 'first_name' is neither part of the GROUP BY 
+   nor wrapped in an aggregate function. SQL doesn't know how to handle this column. */
+SELECT 
+    country,
+    first_name,
+    SUM(score) AS total_score
+FROM customers
+GROUP BY country
+
+-- Find the total score and total number of customers for each country
+SELECT 
+    country,
+    SUM(score) AS total_score,
+    COUNT(id) AS total_customers
+FROM customers
+GROUP BY country
