406c444803de7dd0e4d283c7dff160536fdac26d
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-02-18 10:18:58 +0530

    Add queries to calculate average score by country

--- Scripts/01_Query_Data_SELECT.sql
+++ Scripts/01_Query_Data_SELECT.sql
@@ -141,3 +141,28 @@ SELECT
     COUNT(id) AS total_customers
 FROM customers
 GROUP BY country
+
+/* ==============================================================================
+   HAVING
+=============================================================================== */
+
+/* Find the average score for each country
+   and return only those countries with an average score greater than 430 */
+SELECT
+    country,
+    AVG(score) AS avg_score
+FROM customers
+GROUP BY country
+HAVING AVG(score) > 430
+
+/* Find the average score for each country
+   considering only customers with a score not equal to 0
+   and return only those countries with an average score greater than 430 */
+SELECT
+    country,
+    AVG(score) AS avg_score
+FROM customers
+WHERE score != 0
+GROUP BY country
+HAVING AVG(score) > 430
+
