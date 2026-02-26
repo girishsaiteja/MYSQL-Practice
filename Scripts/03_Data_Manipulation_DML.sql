85f6c4931e3972643b4526abd62db2f232f620d7
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-02-26 15:58:28 +0530

    Insert new records into customers table
    Added new records to the customers table with NULL values.

--- Scripts/03_Data_Manipulation_DML.sql
+++ Scripts/03_Data_Manipulation_DML.sql
@@ -11,3 +11,13 @@
 =================================================================================
 */
 
+
+/* ============================================================================== 
+   INSERT
+=============================================================================== */
+/* #1 Method: Manual INSERT using VALUES */
+-- Insert new records into the customers table
+INSERT INTO customers (id, first_name, country, score)
+VALUES 
+    (6, 'Anna', 'USA', NULL),
+    (7, 'Sam', NULL, 100)
