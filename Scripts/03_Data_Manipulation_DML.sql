ecc89e93d3911d71001a6198bbf909292f8dc61c
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-03-06 19:31:43 +0530

    Update 03_Data_Manipulation_DML.sql

--- Scripts/03_Data_Manipulation_DML.sql
+++ Scripts/03_Data_Manipulation_DML.sql
@@ -80,3 +80,23 @@ WHERE score IS NULL
 SELECT *
 FROM customers
 WHERE score IS NULL
+
+/* ============================================================================== 
+   DELETE
+=============================================================================== */
+
+-- Select customers with an ID greater than 5 before deleting
+SELECT *
+FROM customers
+WHERE id > 5
+
+-- Delete all customers with an ID greater than 5
+DELETE FROM customers
+WHERE id > 5
+
+-- Delete all data from the persons table
+DELETE FROM persons
+
+-- Faster method to delete all rows, especially useful for large tables
+TRUNCATE TABLE persons
+
