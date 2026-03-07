376f950b46b93ddae799bca17d9cd43c3fa2d45f
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-03-07 19:26:30 +0530

    Create SQL script for data filtering techniques
    Added SQL script for filtering data with examples.

---
+++ Scripts/04_Filtering_Data.sql
@@ -0,0 +1,33 @@
+/* ============================================================================== 
+   SQL Filtering Data
+-------------------------------------------------------------------------------
+   This document provides an overview of SQL filtering techniques using WHERE 
+   and various operators for precise data retrieval.
+
+   Table of Contents:
+     1. Comparison Operators
+        - =, <>, >, >=, <, <=
+     2. Logical Operators
+        - AND, OR, NOT
+     3. Range Filtering
+        - BETWEEN
+     4. Set Filtering
+        - IN
+     5. Pattern Matching
+        - LIKE
+=================================================================================
+*/
+
+/* ============================================================================== 
+   COMPARISON OPERATORS
+=============================================================================== */
+
+-- Retrieve all customers from Germany.
+SELECT *
+FROM customers
+WHERE country = 'Germany'
+
+-- Retrieve all customers who are not from Germany.
+SELECT *
+FROM customers
+WHERE country <> 'Germany'
