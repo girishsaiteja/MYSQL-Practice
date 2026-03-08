8fb20c099e4b3951e4d004bfef5d9b46c60920be
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-03-08 17:01:25 +0530

    Add SQL queries for customer score filtering
    Added SQL queries to filter customers based on score.

--- Scripts/04_Filtering_Data.sql
+++ Scripts/04_Filtering_Data.sql
@@ -31,3 +31,23 @@ WHERE country = 'Germany'
 SELECT *
 FROM customers
 WHERE country <> 'Germany'
+
+-- Retrieve all customers with a score greater than 500.
+SELECT *
+FROM customers
+WHERE score > 500
+
+-- Retrieve all customers with a score of 500 or more.
+SELECT *
+FROM customers
+WHERE score >= 500
+
+-- Retrieve all customers with a score less than 500.
+SELECT *
+FROM customers
+WHERE score < 500
+
+-- Retrieve all customers with a score of 500 or less.
+SELECT *
+FROM customers
+WHERE score <= 500
