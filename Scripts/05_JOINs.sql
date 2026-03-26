5bc4737d8634c5d784556037656afca79534d5c6
Girish <148739314+girishsaiteja@users.noreply.github.com>
2026-03-26 21:07:52 +0530

    Create 05_JOINs.sql

---
+++ Scripts/05_JOINs.sql
@@ -0,0 +1,21 @@
+/* ==============================================================================
+   SQL Joins 
+-------------------------------------------------------------------------------
+   This document provides an overview of SQL joins, which allow combining data
+   from multiple tables to retrieve meaningful insights.
+
+   Table of Contents:
+     1. Basic Joins
+        - INNER JOIN
+        - LEFT JOIN
+        - RIGHT JOIN
+        - FULL JOIN
+     2. Advanced Joins
+        - LEFT ANTI JOIN
+        - RIGHT ANTI JOIN
+        - ALTERNATIVE INNER JOIN
+        - FULL ANTI JOIN
+        - CROSS JOIN
+     3. Multiple Table Joins (4 Tables)
+=================================================================================
+*/
